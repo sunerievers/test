09c6ffe1e437be1002f57e1a261741dce61e1300
sunerievers <sune.rievers@gmail.com>
2021-08-19 21:26:15 +0200

    Update Extra.cs

--- TestConsole/Extra.cs
+++ TestConsole/Extra.cs
@@ -1,16 +1,46 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TestConsole
 {
-    class Extra
+    public class Extra
     {
         public static void Hello()
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public int GetNumber()
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+            var d = a * b * c;
+
+            for (int i = 0; i < 10; ++i)
+            {
+                d = -d;
+            }
+
+            switch(d)
+            {
+                case 0:
+                    return 0;
+
+                case 1:
+                    return 1;
+
+                case 2:
+                    return 2;
+
+                default:
+                    return -1;
+            }
         }
     }
 }
