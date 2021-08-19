1c2545d40f7e2a9a99e6b1ebe8eace778aedb143
Sune Rievers <sri@netcompany.com>
2021-08-19 21:33:28 +0200

    a

--- TestConsole/Extra.cs
+++ TestConsole/Extra.cs
@@ -42,5 +42,23 @@ namespace TestConsole
                     return -1;
             }
         }
+        public int GetNumber2()
+        {
+            var a = 1; var b = 2; var c = 3; var d = a * b * c;
+
+            for (int i = 0; i < 10; ++i)
+                d = -d;
+
+            switch (d)
+            {
+                case 0: return 0;
+
+                case 1: return 1;
+
+                case 2: return 2;
+
+                default: return -1;
+            }
+        }
     }
 }
