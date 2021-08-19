9fd0308f85605b164aa3a4bed9f1c1d8d9301a15
sunerievers <sune.rievers@gmail.com>
2021-08-19 20:44:01 +0200

    Add files via upload

---
+++ TestConsole/Extra.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Extra
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
