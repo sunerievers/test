a78516854211e3d7accbd5b8950d9cf70c654e4d
sunerievers <sune.rievers@gmail.com>
2021-08-19 20:49:37 +0200

    Add files via upload

---
+++ TestConsoleTests/ExtraTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestConsole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole.Tests
+{
+    [TestClass()]
+    public class ExtraTests
+    {
+        [TestMethod()]
+        public void HelloTest()
+        {
+            Assert.IsTrue(true); //HACK
+        }
+    }
+}
