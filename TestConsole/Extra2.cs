bdf61f2e728c09ae963febf59b04615c6359453b
Sune Rievers <sri@netcompany.com>
2021-08-19 21:37:43 +0200

    cdsadas

--- TestConsole/Extra2.cs
+++ TestConsole/Extra2.cs
@@ -10,38 +10,6 @@ namespace TestConsole
 {
     public class Extra2
     {
-        public static void Hello()
-        {
-            Console.WriteLine("Hello World!");
-        }
-
-        public int GetNumber()
-        {
-            var a = 1;
-            var b = 2;
-            var c = 3;
-            var d = a * b * c;
-
-            for (int i = 0; i < 10; ++i)
-            {
-                d = -d;
-            }
-
-            switch(d)
-            {
-                case 0:
-                    return 0;
-
-                case 1:
-                    return 1;
-
-                case 2:
-                    return 2;
-
-                default:
-                    return -1;
-            }
-        }
         public int GetNumber2()
         {
             var a = 1; var b = 2; var c = 3; var d = a * b * c;
@@ -60,5 +28,38 @@ namespace TestConsole
                 default: return -1;
             }
         }
+
+        public int GetNumber()
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+            var d = a * b * c;
+
+            for (int i = 0; i < 10; ++i)
+            {
+                d = -d;
+            }
+
+            switch (d)
+            {
+                case 0:
+                    return 0;
+
+                case 1:
+                    return 1;
+
+                case 2:
+                    return 2;
+
+                default:
+                    return -1;
+            }
+        }
+        public static void Hello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
     }
 }
